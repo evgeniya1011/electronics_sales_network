5aefe846706e8b31c8148754183310af0f28a01f
evgeniya1011 <zhdavydova@mail.ru>
2024-01-24 00:53:27 +0300

    create csu.py

---
+++ users/management/commands/csu.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='admin@yandex.ru',
+            first_name='Admin',
+            last_name='Adminov',
+            is_active=True,
+            is_staff=True,
+            is_superuser=True
+        )
+
+        user.set_password('123qwe456rty')
+        user.save()
